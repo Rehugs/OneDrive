d45287cf4009e2f1cf6632795b00fcf472f87c88
reagin <reagin@163.com>
2024-07-06 21:02:28 +0800

    chore: Update language sorting and storage configuration

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'es', 'hi', 'id', 'de-DE', 'tr-TR', 'zh-TW', 'zh-CN']
+    locales: ['en', 'zh-CN', 'zh-TW']
   },
   localePath: path.resolve('public/locales'),
   reloadOnPrerender: process.env.NODE_ENV === 'development',
--- src/components/SwitchLang.tsx
+++ src/components/SwitchLang.tsx
@@ -19,16 +19,6 @@ const localeText = (locale: string): string => {
   switch (locale) {
     case 'en':
       return '🇬🇧 English'
-    case 'es':
-      return '🇪🇸 Español'
-    case 'hi':
-      return '🇮🇳 हिन्दी'
-    case 'id':
-      return '🇮🇩 Indonesia'
-    case 'de-DE':
-      return '🇩🇪 Deutsch'  
-    case 'tr-TR':
-      return '🇹🇷 Türkçe'
     case 'zh-CN':
       return '🇨🇳 简体中文'
     case 'zh-TW':
